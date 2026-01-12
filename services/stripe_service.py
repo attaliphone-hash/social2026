4fe562a1b0eab3adc865abdee2954d33db96a25e
ATTAL SYLVAIN <attaliphone@gmail.com>
2026-01-12 17:06:21 +0100

    app 85

--- services/stripe_service.py
+++ services/stripe_service.py
@@ -1,13 +1,18 @@
 import stripe
 import os
 import streamlit as st
-
-# Configuration de la clé API
-stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+from dotenv import load_dotenv
 
 def create_checkout_session(plan_type):
     """Crée une session de paiement Stripe pour l'abonnement"""
-    # IDs Stripe (Hardcodés pour ta version actuelle)
+    
+    # CHARGEMENT DE SÉCURITÉ : On s'assure que les variables sont là
+    load_dotenv()
+    
+    # On configure la clé JUSTE AVANT l'appel, pour être sûr qu'elle est chargée
+    stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+    
+    # IDs Stripe
     price_id = "price_1SnaTDQZ5ivv0RayXfKqvJ6I" if plan_type == "Mensuel" else "price_1SnaUOQZ5ivv0RayFnols3TI"
     
     try:
